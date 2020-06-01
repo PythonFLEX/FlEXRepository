af41c064700e01f9bbde0844b96e4c81926828d0
AnatasBlai <flippy23412@yandex.ru>
2020-06-02 00:44:58 +0500

    release 1.0

---
+++ all.py
@@ -0,0 +1,17 @@
+import max_elem
+import sort_list
+import two_lists
+
+print("1. Нахождение максимального элемента в списке")
+print("2. Сортировка только простых элементов в списке")
+print("3. Сравнение суммы максимального и минимального элементов двух списков")
+print()
+choose = int(input("Выберите действие:"))
+if choose == 1:
+    print("Максимальный элемент:", max_elem.number_max())
+elif choose == 2:
+    print("Отсортированный список:", sort_list.number_sort_odd())
+elif choose == 3:
+    print(two_lists.comparison())
+else:
+    print("Введено неверное значение")
---
+++ sort_list.py
@@ -0,0 +1,22 @@
+def number_sort_odd():
+    # Массив для записи всех чисел.
+    numbers = list()
+    n = int(input("Введите число эллементов: "))
+    # Цикл для записи элементов в список.
+    for i in range(n):
+       # Запись эллементов в список.
+        numb = int(input("Введите эллемент: "))
+        numbers.append(numb)
+
+
+        
+    # Сортировка нечетных элементов
+    number_sort = sorted([i for i in numbers if i % 2 != 0])
+    odd_int = 0
+    for i in range(len(numbers)):
+        if numbers[i] % 2 != 0:
+            numbers[i] = number_sort[odd_int]
+            odd_int += 1
+    # Возвращение отсортированного массива в тело программы.
+    return numbers
+
